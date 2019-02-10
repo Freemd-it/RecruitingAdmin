d3ef9d15e4d07c8d0030538e95675d0b91498b3e
urbanscenery <urbanscenery@gmail.com>
2019-02-11 03:52:46 +0900

    Add filtering at Schedule

--- admin-backend/controllers/Schedule.Ctrl.js
+++ admin-backend/controllers/Schedule.Ctrl.js
@@ -5,7 +5,7 @@ const moment = require('moment');
 
 const userScheduleInfo = (userObj) => {
     console.log(userObj)
-    if(!userObj.interview_info) return;
+    if (!userObj.interview_info) return;
     const interviewTime = ["10:00 ~ 12:00", "12:00 ~ 14:00", "14:00 ~ 16:00", "16:00 ~ 18:00"];
 
     const first_department = Code.getDepartmentName(Number(userObj.basic_info.department + '00')) + ' ' + Code.getTeamName(Number(userObj.basic_info.department + userObj.basic_info.team));
@@ -14,17 +14,17 @@ const userScheduleInfo = (userObj) => {
     const saturday = [];
     const sunday = [];
 
-    for(let i = 2 ; i < 4 ; i++) {
+    for (let i = 2; i < 4; i++) {
         let interview_data = {
-            interview_time : interviewTime[i],
-            interview_available : userObj.interview_info[0].interview_time.includes(interviewTime[i]),
+            interview_time: interviewTime[i],
+            interview_available: userObj.interview_info[0].interview_time.includes(interviewTime[i]),
         }
         saturday.push(interview_data);
     }
-    for(let i = 0 ; i < 4 ; i++) {
+    for (let i = 0; i < 4; i++) {
         let interview_data = {
-            interview_time : interviewTime[i],
-            interview_available : userObj.interview_info[0].interview_time.includes(interviewTime[i]),
+            interview_time: interviewTime[i],
+            interview_available: userObj.interview_info[0].interview_time.includes(interviewTime[i]),
         }
         sunday.push(interview_data);
     }
@@ -44,24 +44,77 @@ const userScheduleInfo = (userObj) => {
     }
 }
 
+const matchSearchIndexandSchemaKey = (searchIndex, searchKeyword) => {
+    if (searchIndex === 'name') {
+        return {
+            $and: [
+                {
+                    "support_status": {$gte: 201}
+                },
+                {
+                    "basic_info.user_name": new RegExp(searchKeyword),
+                }
+            ]
+        };
+    }
+    if (searchIndex === 'department') {
+        return {
+            $and: [
+                {
+                    "support_status": {$gte: 201}
+                },
+                {
+                    $or: [
+                        {
+                            "basic_info.department": Code.getDepartmentCode(searchKeyword),
+                        },
+                        {
+                            "basic_info.secondary_department": Code.getDepartmentCode(searchKeyword),
+                        }
+                    ]
+                }
+            ]
+            
+        };
+    }
+    if (searchIndex === 'age') {
+        const birthYear = age_birthDate_convert(searchKeyword);
+        return {
+            $and: [
+                {
+                    "support_status": {$gte: 201}
+                },
+                {
+                    "basic_info.birth_date": new RegExp(String(birthYear)),
+                }
+            ]
+        }
+    }
+}
 
-const getScheduleUserList = async(req, res) => {
-  try {
-    const userList = await User
-      .find({"support_status": {$gte: 201}})
-      .select("basic_info interview_info")
-      .sort({_id: -1})
-      .exec();
-      
-    const resUserList = userList.map(user => userScheduleInfo(user));
-    res.status(200).json({message: "Successful get schedule list", result: resUserList});
+const getScheduleUserList = async (req, res) => {
+    let findOption = { "support_status": { $gte: 201 } }
+    if (req.query.type && req.query.q) {
+        const searchIndex = req.query.type;
+        const searchKeyword = req.query.q;
+        findOption = matchSearchIndexandSchemaKey(searchIndex, searchKeyword);
+    }
+    try {
+        const userList = await User
+            .find(findOption)
+            .select("basic_info interview_info")
+            .sort({ _id: -1 })
+            .exec();
 
-  } catch(e) {
-    console.log(e);
-    res.status(500).json({message : JSON.stringify(e), result: null,});
-  }
+        const resUserList = userList.map(user => userScheduleInfo(user));
+        res.status(200).json({ message: "Successful get schedule list", result: resUserList });
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: JSON.stringify(e), result: null, });
+    }
 }
 
 module.exports = {
-    getScheduleList : getScheduleUserList,
+    getScheduleList: getScheduleUserList,
 }
