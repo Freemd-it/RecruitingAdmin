deac0b903aa72ee9d9cc484ff3de0b191f0bbfba
unknown <pilju413@gmail.com>
2019-08-03 19:24:42 +0900

    메모기능 수정

--- admin-frontend/src/lib/api/chart.js
+++ admin-frontend/src/lib/api/chart.js
@@ -2,6 +2,6 @@ import axiosCreate from '../defaultAxios';
 
 export const getChartData= (ctx) => 
   axiosCreate().get(`/admin/statistics`)
-    .then(res => ctx.setState({ rows: res.data.result}))
+    .then(res => ctx.setState({ chartData: res.data.result}))
     .catch(err => err)
 
