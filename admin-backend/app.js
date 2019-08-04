0b383c1680cd5d2ae504197f25ee117bd5303fa1
urbanscenery <urbanscenery@gmail.com>
2019-08-04 19:28:52 +0900

    Add swagger-express-generator

--- admin-backend/app.js
+++ admin-backend/app.js
@@ -18,8 +18,8 @@ const expressSwagger = require('express-swagger-generator')(app);
 let options = {
   swaggerDefinition: {
       info: {
-          description: 'This is a sample server',
-          title: 'Swagger',
+          description: '프리메드 Recruiting Admin 서버 API',
+          title: 'Freemed Recruiting Admin',
           version: '1.0.0',
       },
       host: 'localhost:3001',
