0fd981802d9d576549459e5389f1994d2082a3e2
Hyeong Jun Kim <hjkim2246@gmail.com>
2019-08-07 19:41:04 +0900

    프로젝트 추가, 삭제 API 추가

--- admin-backend/controllers/Project.Ctrl.js
+++ admin-backend/controllers/Project.Ctrl.js
@@ -1,6 +1,8 @@
 const Project = require('../models/ProjectModel');
+const mongoose = require('mongoose');
 
 const getProjectList = async (req, res) => {
+  console.log('get project');
   try {
     const projectData = await Project
     .find({})
@@ -13,6 +15,50 @@ const getProjectList = async (req, res) => {
   }
 }
 
+const postProject = async (req, res) => {
+  const { 
+    projectName, 
+    projectDesc
+  } = req.body;
+
+  if (!projectName || !projectDesc)
+    return res.status(500).json({
+      message: 'invalied value',
+      result: null,
+    });
+
+  try {
+    const project = new Project({ 
+      projectName, 
+      projectDesc
+    });
+
+    const savedProject = await project.save();
+    res.status(201).json({ message : "Success", result: savedProject});
+  } catch(e) {
+    res.status(500).json({ message: JSON.stringify(e) , result: null,});
+  }
+}
+
+const deleteProject = async (req, res) => {
+  const { projectid } = req.body;
+  if (!projectid )
+    return res.status(500).json({
+      message: 'invalied value',
+      result: null,
+    });
+
+  try {
+    const result = Project.findByIdAndDelete(projectid).exec();
+    res.status(201).json({ message : "Success"});
+  } catch(e) {
+    res.status(500).json({ message: JSON.stringify(e) , result: null,});
+  }
+}
+
+
 module.exports = {
   getProjectList,
+  postProject,
+  deleteProject
 }
