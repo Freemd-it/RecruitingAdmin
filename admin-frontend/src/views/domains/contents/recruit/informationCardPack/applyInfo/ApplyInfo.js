fbdf927bd27921a947ba8eeae4f79c17779e48bf
unknown <pilju413@gmail.com>
2019-08-08 04:33:04 +0900

    지원서관리->인터뷰타임나오게

--- admin-frontend/src/views/domains/contents/recruit/informationCardPack/applyInfo/ApplyInfo.js
+++ admin-frontend/src/views/domains/contents/recruit/informationCardPack/applyInfo/ApplyInfo.js
@@ -5,54 +5,51 @@ import InfoIcon from '@material-ui/icons/Info'
 import InformationCard from 'views/contexts/modal/card';
 
 const InterviewInfo = ({data}) => {
-  const interviewTable = [
-    [
-      "14:00 ~ 16:00",
-      "15:00 ~ 17:00",
-      "16:00 ~ 18:00",
-      "17:00 ~ 19:00",
-    ],
-    [
-      "10:00 ~ 12:00",
-      "11:00 ~ 13:00",
-      "13:00 ~ 15:00",
-      "14:00 ~ 16:00",
-      "15:00 ~ 17:00",
-      "16:00 ~ 18:00",
-    ],
-  ]
+  const { interviewTime } = JSON.parse(localStorage.getItem('recruitMeta'));
+  const interviewTable = [];
+  const dateList = [];
+  for(const key in interviewTime) {
+    interviewTable.push(interviewTime[key]);
+  }
+
   const title = (
     <span className="Title">
       <InfoIcon className="Icon" /> 지원 정보
-    </span>)
+    </span>
+  );
 
+
+  const interviewEl = data.map((item, index) => {
+    const { interviewWeek, interviewTime, interviewDate } = item;
+    return (
+      <div 
+        key={`${interviewWeek}__${index}`} 
+        className="SubContent"
+      >
+        <div>
+          <span> {interviewWeek}요일 - { moment(`${interviewDate}`).format("M월 D일")}</span>
+          {
+            interviewTable[index].map((time, index) => {
+              const result = []
+              let cnt = 0
+              interviewTime.forEach((_time, _index) => {
+                if(time === _time) {
+                  result.push(<div key={`interviewTime__${interviewWeek}__${_time}`} className="InterviewCell Selected">{_time}</div>)
+                  cnt++;
+                }
+              })
+              cnt === 0 && result.push(<div key={`interviewTime__${interviewWeek}__${time}`}className="InterviewCell">{time}</div>)
+              return result
+            })
+          }
+        </div>
+      </div>
+    );
+  });
   const content = (
     <div className="Content">
       <div className="Head-3">인터뷰 가능 시간</div>
-      {
-        data.map(({interview_week, interview_time, interview_date}, index) => (
-          interview_date &&
-          <div className="SubContent">
-            <div>
-              <div> {interview_week}요일 - { moment(`${interview_date}`).format("M월 D일")}</div>
-                {
-                  interviewTable[index].map((time, index) => {
-                    const result = []
-                    let cnt = 0
-                    interview_time.forEach((_time, _index) => {
-                      if(time === _time) {
-                        result.push(<div className="InterviewCell Selected">{_time}</div>)
-                        cnt++;
-                      }
-                    })
-                    cnt === 0 && result.push(<div className="InterviewCell">{time}</div>)
-                    return result
-                  })
-                }
-              </div>
-          </div>
-        ))
-      }
+        {interviewEl}
       </div>
     )
     
