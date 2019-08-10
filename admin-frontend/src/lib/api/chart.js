be80275bf3d36e72ba4efa84cc0febb7fe338083
Hyeong Jun Kim <hjkim2246@gmail.com>
2019-08-10 15:40:15 +0900

    chart 에러 해결 및 도커파일 추가

--- admin-frontend/src/lib/api/chart.js
+++ admin-frontend/src/lib/api/chart.js
@@ -2,6 +2,9 @@ import axiosCreate from '../defaultAxios';
 
 export const getChartData= (ctx) => 
   axiosCreate().get(`/admin/statistics`)
-    .then(res => ctx.setState({ chartData: res.data.result}))
+    .then(res => {
+      console.log('chart api call', res.data.result);
+      ctx.setState({ chartData: res.data.result});
+    })
     .catch(err => err)
 
