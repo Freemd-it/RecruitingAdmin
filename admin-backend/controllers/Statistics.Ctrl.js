aa206d5b3ece7be9a9a84f312a4022e69a9b6520
Hyeong Jun Kim <hjkim2246@gmail.com>
2019-08-28 11:09:50 +0900

    본부별, 팀 별 지원자 현황 파악 기능 추가 (#47)

--- admin-backend/controllers/Statistics.Ctrl.js
+++ admin-backend/controllers/Statistics.Ctrl.js
@@ -1,15 +1,119 @@
 const Statistics = require('../models/StatisticsModel');
+const User = require('../models/UserModel2');
+const DepartmemtMeta = require('../models/DepartmentMetaModel');
+
 
 const getStat = async(req, res) => {
     try {
-        const stat = await Statistics
-                                .findOne({batch: 20});
+        const stat = await Statistics.findOne({batch: 20});
         res.status(200).json({message: "Successful get statistics", result: stat});
     } catch(e) {
         res.status(500).json({message : JSON.stringify(e), result: null, });
     }
 }
 
+const initApplierStat = (departmentMeta) => {
+    let applierStat = {
+        totalFirst: 0,
+        totalSecond: 0,
+        stats: []
+    };
+    for(let i=0; i<departmentMeta.length; i++) {
+        const department = departmentMeta[i];
+        // console.log(department.teams);
+        if (department.departmentName==='공통') {
+            continue;
+        }
+
+        let newDepartment = {
+            departmentName: department.departmentName,
+            first: 0,
+            second: 0,
+            teams: []
+        }
+
+        // console.log(department.teams);
+        for(let j=0; j<department.teams.length; j++) {
+            const team = department.teams[j];
+            if (team.teamName==='공통' && department.departmentName!=='해외의료사업본부' && department.departmentName!=='보건교육사업본부') {
+                continue;
+            }
+            let newTeam = {
+                teamName: team.teamName,
+                first: 0,
+                second: 0
+            }
+            newDepartment.teams.push(newTeam);
+        }
+        applierStat.stats.push(newDepartment);
+    }
+    return applierStat
+}
+
+const countApplier = (departments, applierStat, index) => {
+    const totalCount = index===0 ? 'totalFirst' : 'totalSecond';
+    const applyCount = index===0 ? 'first' : 'second';
+    const departName = departments[index].departmentName;
+    const teamName = departments[index].teamName;
+    if (departName==='') {
+        return;
+    }
+
+    for(let i=0; i<applierStat.stats.length; i++) {
+        if (applierStat.stats[i].departmentName!==departName) {
+            continue
+        }
+        applierStat.stats[i][applyCount] += 1;
+        applierStat[totalCount] += 1;
+        for(let j=0; j<applierStat.stats[i].teams.length; j++) {
+            if (applierStat.stats[i].teams[j].teamName===teamName) {
+                applierStat.stats[i].teams[j][applyCount] += 1;
+                break;
+            }
+        }
+        break;
+    }
+}
+
+const getApplierStat = async (req, res) => {
+    const userFindOption = {
+        batch: req.params.batch,
+        supportStatus: 201,
+    }
+
+    const departmentFindOption = {
+        batch: req.params.batch,
+    }
+
+    try {
+        const departmentMeta = await DepartmemtMeta
+        .find(departmentFindOption)
+        .select("departmentName teams")
+        .sort({ _id: -1 })
+        .exec();
+        
+        let applierStat = initApplierStat(departmentMeta);
+
+        const userList = await User
+        .find(userFindOption)
+        .select("batch basicInfo")
+        .sort({ _id: -1 })
+        .exec();
+
+        userList.forEach(({basicInfo}) => {
+            const { departments } = basicInfo;
+            for (let i=0; i<2; i++) {
+                countApplier(departments, applierStat, i);
+            }
+        })
+        res.status(200).json({message: "Successful get applier statistics", result: applierStat});
+    } catch(e) {
+        console.log(e);
+        res.status(500).json({message : JSON.stringify(e), result: null, });
+    }
+}
+
 module.exports = {
-    getStat: getStat
+    getStat: getStat,
+    getApplierStat: getApplierStat
 };
--- admin-frontend/src/views/domains/contents/dashboard/DashBoard.js
+++ admin-frontend/src/views/domains/contents/dashboard/DashBoard.js
@@ -4,30 +4,29 @@ import './DashBoard.scss'
 
 class DashBoard extends Component {
   render() {
-    const { pie, radar } = this.props.chartData
-    
-    const pieChart = (
-      <div className="item" key={1}>
-        <Pie
-          height={150}
-          data={pie.data}
-          options={pie.options}
-          legend={pie.legend}
-        />
-      </div>
-    )
+    const applierStat = this.props.applierStat;
+    // const { pie, radar } = this.props.chartData;
+    // const pieChart = (
+    //   <div className="item" key={1}>
+    //     <Pie
+    //       height={150}
+    //       data={pie.data}
+    //       options={pie.options}
+    //       legend={pie.legend}
+    //     />
+    //   </div>
+    // )
 
-    const radarChart = (
-      <div className="item" key={2}>
-          <Radar
-            height={150}
-            data={radar.data}
-            options={radar.options}
-            legend={radar.legend}
-          />
-      </div>
-    )
-
+    // const radarChart = (
+    //   <div className="item" key={2}>
+    //       <Radar
+    //         height={150}
+    //         data={radar.data}
+    //         options={radar.options}
+    //         legend={radar.legend}
+    //       />
+    //   </div>
+    // )
     // const barChart = (
     //   <div className="item" key={3}>
     //     <Bar
@@ -39,20 +38,74 @@ class DashBoard extends Component {
     //     />
     //   </div>
     // );
-
+    const currentDate = new Date();
+    const dateStr = `${currentDate.getFullYear()}. ${currentDate.getMonth()+1}-${currentDate.getDate()}`;
     return (
         <div className="chart-wrapper">
-          <div className="title"> 지원자 현황 - ({this.props.chartData.lastUpdated})</div>
+          <div className="title"> 지원자 현황 - ({dateStr} 기준)</div>
           <div className="chart-container">
-            <div className="chart-sub-container">
-              {pieChart}
-              {/* {barChart} */}
-              {radarChart}
-            </div>
+          {applierStat ? <ApplierStatTable applierStat={applierStat} />:""}:
           </div>
         </div>
       );
   }
+}
+
+const ApplierStatTable = ({applierStat}) => {
+  return (
+    <div className="applier-stat-container">
+      <div className="total-count-container">
+        <span className="total-count">전체 1지망 지원 : {applierStat.totalFirst} 명</span>
+        <span className="total-count">전체 2지망 지원 : {applierStat.totalSecond} 명</span>
+      </div>
+      <table>
+      <thead>
+        <tr>
+          <th>본부명</th>
+          <th>본부 1지망</th>
+          <th>본부 2지망</th>
+          <th>팀 명</th>
+          <th>팀 1지망</th>
+          <th>팀 2지망</th>
+        </tr>
+      </thead>
+      <tbody>
+        {applierStat.stats.map(stat => {
+          const teamCount = stat.teams.length;
+          return(
+            <>
+            <tr>
+              <td rowSpan={teamCount}>{stat.departmentName}</td>
+              <td rowSpan={teamCount}>{stat.first}</td>
+              <td rowSpan={teamCount}>{stat.second}</td>
+              <td>{stat.teams[0].teamName}</td>
+              <td>{stat.teams[0].first}</td>
+              <td>{stat.teams[0].second}</td>
+            </tr>
+            { teamCount > 1 ? <ApplierStatRows teams={stat.teams.splice(1, stat.teams.length)}/> : ""}
+            </>
+          )
+        })}
+      </tbody>
+      </table>
+    </div>
+  )
+}
+
+const ApplierStatRows = ({teams}) => {
+  return (
+    <>
+      {teams.map(team => {
+        return (
+          <tr>
+            <td>{team.teamName}</td>
+            <td>{team.first}</td>
+            <td>{team.second}</td>
+          </tr>
+        )
+      })}
+    </>
+  )
 }
 
 export default DashBoard
